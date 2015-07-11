0b8de25735294c7a60a6f880ad00cd35449f93e6
tay <tay@erratik.ca>
2015-07-11 04:02:17 -0400

    cleaning up the structure?

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,22 @@
+var app = angular.module('app', [
+    'ui.router', 
+    'angularMoment', 
 
-    var app = angular.module('app', ['ui.router', 'angularMoment', 'controllers.Settings', 'directives.removeNamespace', 'directives.configureNamespace', 'directives.disconnectNamespace', 'directives.customProperties', 'directives.detailsNamespace']);
-    // use unix timestamps in angular views
-    app.constant('angularMomentConfig', {
-        preprocess: 'unix', // optional
-    }).config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
-        $urlRouterProvider.otherwise('/');
-        $stateProvider.state('settings', {
-            url: '/',
-            controller: 'settingsController as settings',
-            templateUrl: 'templates/tpl--settings.html'
-        }).state('profiles', {
-            url: '/profiles',
-            controller: 'settingsController as settings',
-            templateUrl: 'templates/tpl--profiles.html'
-        });
-    }]);
+    'controllers.Settings',
+    'controllers.Profiles'
+]);
+// use unix timestamps in angular views
+app.constant('angularMomentConfig', {
+    preprocess: 'unix', // optional
+}).config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state('settings', {
+        url: '/',
+        controller: 'settingsController as settings',
+        templateUrl: 'templates/tpl--settings.html'
+    }).state('profiles', {
+        url: '/profiles',
+        controller: 'settingsController as settings',
+        templateUrl: 'templates/tpl--profiles.html'
+    });
+}]);
